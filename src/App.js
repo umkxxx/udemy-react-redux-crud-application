e595fa5b1b02389bada8346aa4c96ee105f1eb64
KOMEI SATO <sato@kappuku.jp>
2021-01-09 17:38:30 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 const App = () => {
-  // return (
-  //   <React.Fragment>
-  //     <User name={"Taro"} age={14}/>
-  //     <User name={"Jiro"} age={11}/>
-  //   </React.Fragment>
-  // );
-  const hogehgoe = [
-    {name:"TARO", age:24},
-    {name:"JIRO", age:21},
-    {name:"SABURO", age:3}
-  ]
   return (
     <React.Fragment>
-      {
-        hogehgoe.map((h, i) => {
-          return <User name={h.name} age={h.age} key={i} />
-        })
-      }
+      <Counter />
     </React.Fragment>
   );
 }
-function User(props) {
-  return (
-    <React.Fragment>
-      <div>I am {props.name}, and {props.age} years old.</div>
-    </React.Fragment>
-  );
-}
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = {count:0}
+  }
+  handlePlusButton = () => {
+    // this.state = {count:this.state.count+1}   //renderが走らない
+    this.setState({count:this.state.count+1})
+  }
+  handleMinusButton = () => {
+    this.setState({count:this.state.count-1})
+  }
+
+  render(){
+    console.log("render")
+    return (
+      <React.Fragment>
+        <div>counter:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
