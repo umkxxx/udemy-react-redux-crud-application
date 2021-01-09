0a3eabcf44fc3598cbdaa0888772e71ac35a6354
KOMEI SATO <sato@kappuku.jp>
2021-01-09 17:05:17 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
 import React, { Component } from 'react'
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <h1 className="hoge1">className</h1>
-//       <h1 class="hoge2">class</h1>
-//       <label htmlFor="bar1">htmlFor</label>
-//       <label for="bar2">for</label>
-//       <input type="text" onClick={() => {console.log("onClick-1")}}/>
-//       <input type="text" onClick={() => {console.log("onClick-2")}}/>
-//       <input type="text" onChange={() => {console.log("onChange-3")}}/>
-//     </React.Fragment>
-//   );
-// }
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <h1>Component</h1>
-//         <h3 className="hoge1">className</h3>
-//         <h3 class="hoge2">class</h3>
-//         <label htmlFor="bar1">htmlFor</label>
-//         <label for="bar2">for</label>
-//         <input type="text" onClick={() => {console.log("onClick-1")}}/>
-//         <input type="text" onClick={() => {console.log("onClick-2")}}/>
-//         <input type="text" onChange={() => {console.log("onChange-3")}}/>
-//       </React.Fragment>
-//     );
-//   }
-// }
 const App = () => {
+  // return (
+  //   <React.Fragment>
+  //     <User name={"Taro"} age={14}/>
+  //     <User name={"Jiro"} age={11}/>
+  //   </React.Fragment>
+  // );
+  const hogehgoe = [
+    {name:"TARO", age:"24"},
+    {name:"JIRO", age:"21"},
+    {name:"SABURO"}
+  ]
   return (
     <React.Fragment>
-      <h1 className="hoge1">const App</h1>
-      <h1 class="hoge2">class</h1>
-      <label htmlFor="bar1">htmlFor</label>
-      <label for="bar2">for</label>
-      <input type="text" onClick={() => {console.log("onClick-1")}}/>
-      <input type="text" onClick={() => {console.log("onClick-2")}}/>
-      <input type="text" onChange={() => {console.log("onChange-3")}}/>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        hogehgoe.map((h, i) => {
+          return <User name={h.name} age={h.age} key={i} />
+        })
+      }
     </React.Fragment>
   );
 }
-function Cat() {
+function User(props) {
   return (
     <React.Fragment>
-      <div>Meow!</div>
+      <div>I am {props.name}, and {props.age} years old.</div>
     </React.Fragment>
   );
 }
-// function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "hoge"
-//   );
-// }
+User.defaultProps = {
+  age:3
+}
 
 export default App;
