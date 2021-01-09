fe687b642e192d783853e838c5068f27022e47fe
KOMEI SATO <sato@kappuku.jp>
2021-01-09 16:33:45 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function App() {
   return (
-    <h1>hello world!</h1>
+    <React.Fragment>
+      <h1 className="hoge1">className</h1>
+      <h1 class="hoge2">class</h1>
+      <label htmlFor="bar1">htmlFor</label>
+      <label for="bar2">for</label>
+      <input type="text" onClick={() => {console.log("onClick-1")}}/>
+      <input type="text" onClick={() => {console.log("onClick-2")}}/>
+      <input type="text" onChange={() => {console.log("onChange-3")}}/>
+    </React.Fragment>
   );
 }
 // function App() {
